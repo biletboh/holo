bf20035a667fe6bf0d932c813aca0b9ab1c06109
biletboh <biletskyboh@gmail.com>
2021-05-03 18:39:12 +0300

    added basic logic that adds word frequency statistics

--- src/analyze.py
+++ src/analyze.py
@@ -1,10 +1,41 @@
 import pandas as pd
+import nltk
+
+
+nltk.download("punkt")
+
+
+word_bag = [
+    "колаборація",
+    "співпраця з нацистами",
+    "допомога євреям",
+    "порятунок жертв",
+    "Голодомор",
+    "польські табори смерті",
+    "концентраційні",
+]
+
+
+def word_frequency(text, term):
+    words: list[str] = nltk.word_tokenize(text.lower())
+    fd = nltk.FreqDist(words)
+    return fd[term]
 
 
 def read_data(sheet_name):
-    data = pd.read_excel("articles.xlsx", sheet_name=sheet_name, header=None)
+    data = pd.read_excel("data/articles.xlsx", sheet_name=sheet_name)
     return data
 
 
+def prepare_data():
+    data = read_data("Історична правда")
+    data.dropna(how="all", axis=1, inplace=True)
+    for term in word_bag:
+        data[term] = data["Текст"].apply(word_frequency, args=(term,))
+    data.to_excel(
+        "data/result.xlsx", sheet_name="Історична правда", index=False
+    )
+
+
 if __name__ == "__main__":
-    print(read_data("історична правда"))
+    prepare_data()
