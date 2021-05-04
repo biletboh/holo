2170797710332b8510f250d4ee244a5eb8c86167
biletboh <biletskyboh@gmail.com>
2021-05-04 18:09:37 +0300

    added tokenizer, stemmer, and lemmatizer

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Description
 
 This is the project that analyzes data about holocaust and holodomor from ukrainian and polish sources.
+
+
+## Dependencies
+Install and run [api_nlp_uk](https://github.com/arysin/api_nlp_uk)
--- src/analyze.py
+++ src/analyze.py
@@ -1,25 +1,46 @@
+import re
 import pandas as pd
 import nltk
+import requests
+
+from tokenize_uk import tokenize_words
+from uk_stemmer import UkStemmer
 
 
 nltk.download("punkt")
+stemmer = UkStemmer()
 
 
-word_bag = [
-    "колаборація",
-    "співпраця з нацистами",
-    "допомога євреям",
-    "порятунок жертв",
-    "Голодомор",
-    "польські табори смерті",
-    "концентраційні",
-]
+CONCEPTS = "колаборація, співпраця, нацисти, допомога, євреї, порятунок, жертви, голодомор, польський, табір, смерть, концентраційний"
 
 
-def word_frequency(text, term):
-    words: list[str] = nltk.word_tokenize(text.lower())
+def clean_text(text):
+    text = re.sub(r"""['’"`�]""", "", text)
+    text = re.sub(r"""([0-9])([\u0400-\u04FF]|[A-z])""", r"\1 \2", text)
+    text = re.sub(r"""([\u0400-\u04FF]|[A-z])([0-9])""", r"\1 \2", text)
+    text = re.sub(r"""[\-.,:+*/_]""", " ", text)
+    return text
+
+
+def ua_tokenize(text):
+    text = clean_text(text)
+    response = requests.post(
+        "http://localhost:8080/lemmatize/", json={"text": text}
+    ).json()
+    lemmatized = response["lemmatizedSentences"]
+    word_list = []
+    for lemma in lemmatized:
+        tokens = tokenize_words(lemma)
+        for token in tokens:
+            stemmed = stemmer.stem_word(token)
+            word_list.append(stemmed)
+    return word_list
+
+
+def word_frequency(text, concept):
+    words: list[str] = ua_tokenize(text.lower())
     fd = nltk.FreqDist(words)
-    return fd[term]
+    return fd[concept]
 
 
 def read_data(sheet_name):
@@ -27,15 +48,16 @@ def read_data(sheet_name):
     return data
 
 
-def prepare_data():
+def process_data():
     data = read_data("Історична правда")
     data.dropna(how="all", axis=1, inplace=True)
-    for term in word_bag:
-        data[term] = data["Текст"].apply(word_frequency, args=(term,))
+    concepts = ua_tokenize(CONCEPTS)
+    for concept in concepts:
+        data[concept] = data["Текст"].apply(word_frequency, args=(concept,))
     data.to_excel(
         "data/result.xlsx", sheet_name="Історична правда", index=False
     )
 
 
 if __name__ == "__main__":
-    prepare_data()
+    process_data()
